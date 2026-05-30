5418ce226c409107fa043cc4fa0c3b1a2e1084a3
fuyixixian-web <fuyixixian@gmail.com>
2026-05-31 01:10:38 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+
   images: {
     unoptimized: true,
   },
+
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
 module.exports = nextConfig;
